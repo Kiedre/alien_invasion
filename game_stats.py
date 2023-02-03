e76e0fb4c033f9fa7fd58c5b61a4a5fb7335dd6f
Local <Local@example.com>
2023-02-03 17:09:39 +0800

    Started project

---
+++ game_stats.py
@@ -0,0 +1,17 @@
+class GameStats:
+    """Track statistics for Alien Invasion."""
+
+    def __init__(self, ai_game):
+        """Initialize statistics."""
+        self.settings = ai_game.settings
+        self.reset_stats()
+        # High score should never be reset.
+        with open("highest_score.data", "r") as highest:
+            for data in highest:
+                self.high_score = int(data)
+
+    def reset_stats(self):
+        """Initialize statistics that can change during the game."""
+        self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
